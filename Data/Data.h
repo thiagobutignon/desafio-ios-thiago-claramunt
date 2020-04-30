b7d14b4d76e6cccf977760aaa5be74f9898ff051
Thiago Butignon <thiagobutignon@gmail.com>
2020-04-30 11:42:40 -0300

    Created Data layer

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Thiago B Claramunt on 30/04/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
