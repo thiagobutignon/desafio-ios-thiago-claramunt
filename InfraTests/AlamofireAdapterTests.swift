00437fb96ec48a33a38726285e092986ed8e6b72
Thiago Butignon <thiagobutignon@gmail.com>
2020-05-06 09:25:25 -0300

    Add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Thiago B Claramunt on 06/05/20.
+//  Copyright © 2020 Thiago B Claramunt. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+     
+    }
+
+
+}
